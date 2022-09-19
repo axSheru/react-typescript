a8d56cf4896906b3d245a5d8bff1ed19a50c41ee
Alex Paredes <alexparedesr95@gmail.com>
2022-09-18 23:42:10 -0500

    FEAT: Created counter using useState and TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Counter } from "./components/Counter";
+
 function App() {
   return (
     <>
       <h1>Hola mundo - TypeScript</h1>
+      <hr />
+      
+      <Counter />
     </>
   );
 }
